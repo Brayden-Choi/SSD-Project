7d758d5a2d63f038e13115a9f5cda384500c4042
BraydenChoi <s10205405@connect.np.edu.sg>
2021-07-17 14:14:45 +0800

    Added Search Function

--- Pages/Games/Index.cshtml.cs
+++ Pages/Games/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MIST.Data;
 using MIST.Models;
@@ -23,10 +24,32 @@ namespace MIST.Pages.Games
         }
 
         public IList<Game> Game { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Genre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Game = await _context.Game.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Game
+                orderby m.Genre
+                select m.Genre;
+
+            var games = from m in _context.Game
+                select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                games = games.Where(s => s.Name.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(Genre))
+            {
+                games = games.Where(x => x.Genre == Genre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Game = await games.ToListAsync();
         }
     }
 }
