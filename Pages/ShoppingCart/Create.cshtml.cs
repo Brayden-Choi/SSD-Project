4478ac128884a0c3251bda79bddf5ee515b31aa1
Yi Da Wong <s10205001@connect.np.edu.sg>
2021-07-27 18:18:54 +0800

    removed features of auditing

--- Pages/ShoppingCart/Create.cshtml.cs
+++ Pages/ShoppingCart/Create.cshtml.cs
@@ -1,51 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using MIST.Data;
-using MIST.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.AspNetCore.Mvc.Rendering;
+//using MIST.Data;
+//using MIST.Models;
 
-namespace MIST.Pages.ShoppingCart
-{
-    public class CreateModel : PageModel
-    {
-        private readonly MIST.Data.MISTDbContext _context;
+//namespace MIST.Pages.ShoppingCart
+//{
+//    public class CreateModel : PageModel
+//    {
+//        private readonly MIST.Data.MISTDbContext _context;
+//        public CreateModel(MIST.Data.MISTDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        public CreateModel(MIST.Data.MISTDbContext context)
-        {
-            _context = context;
-        }
+//        public IActionResult OnGet()
+//        {
+//        ViewData["GameId"] = new SelectList(_context.Game, "ID", "ID");
+//            return Page();
+//        }
 
-        public IActionResult OnGet()
-        {
-        ViewData["GameId"] = new SelectList(_context.Game, "ID", "ID");
-            return Page();
-        }
+//        [BindProperty]
+//        public ShoppingCartItem ShoppingCartItem { get; set; }
 
-        [BindProperty]
-        public ShoppingCartItem ShoppingCartItem { get; set; }
+//        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+//        // more details, see https://aka.ms/RazorPagesCRUD.
+//        public async Task<IActionResult> OnPostAsync()
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return Page();
+//            }
+//            var cartItem = new ShoppingCartItem()
+//            {
+//                ItemId = ShoppingCartItem
+//            }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-            var cartItem = new ShoppingCartItem()
-            {
-                ItemId = ShoppingCartItem
+//            _context.ShoppingCartItem.Add(ShoppingCartItem);
+//            await _context.SaveChangesAsync();
 
-            }
-
-            _context.ShoppingCartItem.Add(ShoppingCartItem);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
-    }
-}
+//            return RedirectToPage("./Index");
+//        }
+//    }
+//}
